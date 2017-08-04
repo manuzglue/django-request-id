af1888e0e0ad25669afe4ddadf6d783e8c41124d
manuzglue <molmedoz@zglue.com>
2017-08-04 14:24:34 -0700

    Update middleware.py

--- request_id/middleware.py
+++ request_id/middleware.py
@@ -4,8 +4,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from . import generate_request_id, local, release_local
 from .conf import REQUEST_ID_HEADER
+import time
 
-
+# TODO: Change name to id
 def get_request_id(request):
     if hasattr(request, 'request_id'):
         return request.request_id
@@ -14,6 +15,11 @@ def get_request_id(request):
     else:
         return generate_request_id()
 
+def get_start_time(request):
+    if hasattr(request, 's'):
+        return request.s
+    else:
+        return time.time()
 
 class RequestIdMiddleware(object):
     def __init__(self, get_response=None):
@@ -21,9 +27,10 @@ class RequestIdMiddleware(object):
 
     def __call__(self, request):
         request_id = get_request_id(request)
+        s = get_start_time(request)
         request.request_id = request_id
         local.request_id = request_id
-
+        local.s = s
         response = self.get_response(request)
 
         release_local(local)
